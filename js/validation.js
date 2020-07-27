f5a77acc9a49e5af4d2e4859a012a319e66aa736
Subhojit paul <subhojit.paul@zensar.com>
2020-07-27 21:03:08 +0530

    Modify and add jpg file

--- js/validation.js
+++ js/validation.js
@@ -2,36 +2,36 @@
 let btn = document.getElementById('btnsubmit')
 
 // let email = document.querySelector('#btn')
-// let  div1 = document.querySelectorAll('.myclass')
+
 // let div=document.getElementsByClassName('myclass')[1]
 function onBtnClick(){
     let userName = document.getElementById('uname').value
 let email = document.getElementById('email').value
 let pass = document.getElementById('pass').value
 let cpass = document.getElementById('cpass').value
-    if(userName.value=='NULL' || userName.value.length<4){
-        div.innerHTML='Username must be3 atleast 3 characters'
+let  div= document.querySelectorAll('.myclass')
+    if(userName.value==null || userName.length<4 || userName.value==""){
+        document.getElementById('name_e').innerHTML='Username must be atleast 3 characters'
     }
     let mailformat = /^w+([.-]?w+)*@w+([.-]?w+)*(.w{2,3})+$/;
     if(email!=null)
     {
-        if(email.value.match(mailformat))
+        if(email.match(mailformat))
         {
         return true;
         }
         else{
-            div.innerHTML= 'Email is not valid'
+            document.getElementById('err_email').innerHTML= 'Email is not valid'
 
         }
     }
     if(pass==null || pass.length<7){
-        div.innerHTML='Password must be atleast 6 characters'
+        
+        document.getElementById('e-pass').innerHTML='Password must be atleast 6 characters'
     }
-    if(cpass!==pass || cpass===null){
-        div.innerHTML='password2 is required'
+    if(cpass!=pass || cpass===null){
+        document.getElementById('e_c').innerHTML='password2 is required'
     }
-    if(userName=='NULL' || userName.length<4){
-        div.innerHTML='Username must be3 atleast 3 characters'
-    }
+    
 }
 btnsubmit.addEventListener('click',onBtnClick)
